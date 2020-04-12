c2dc3094644f951ea141a0917f872648c83fdcd9
carissaeats <58920518+carissaeats@users.noreply.github.com>
2020-04-11 21:44:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } 
+  
+//   else {
+//     x.className = "topnav";
+//   }
+// }
+
 window.onload = function(){
   hide();
   hideDrop();
