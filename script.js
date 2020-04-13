f6725881dbcb7b945a71491e298b7ae41ed57998
carissaeats <58920518+carissaeats@users.noreply.github.com>
2020-04-13 13:30:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,113 +1,115 @@
 window.onload = function(){
-  hide();
-  hideFoods();
-  hidePlaces();
-  hideDrop();
+//   hide();
   hideCaliFoods();
-  hideCaliPlaces();
+  hideCaliPlaces();  
+//   hideFoods();
+//   hidePlaces();
+  hideDrop();
+//   hideCaliFoods();
+//   hideCaliPlaces();
 }
 
-function hide() {
-  var fbphoto1 = document.getElementById('fbphoto1');
-  var fbphoto2 = document.getElementById('fbphoto2');
-  fbphoto1.style.display="none";
-  fbphoto2.style.display="none";
-  document.getElementById("showmorephotosfacebook").innerHTML = "( + ) show more Facebook campus photos!";
-}
+// function hide() {
+//   var fbphoto1 = document.getElementById('fbphoto1');
+//   var fbphoto2 = document.getElementById('fbphoto2');
+//   fbphoto1.style.display="none";
+//   fbphoto2.style.display="none";
+//   document.getElementById("showmorephotosfacebook").innerHTML = "( + ) show more Facebook campus photos!";
+// }
 
-function show() {
-  var fbphoto1 = document.getElementById('fbphoto1');
-  var fbphoto2 = document.getElementById('fbphoto2');
-  fbphoto1.style.display="block";
-  fbphoto2.style.display="block";
-  document.getElementById("showmorephotosfacebook").innerHTML = "( - ) hide these photos!";
-}
+// function show() {
+//   var fbphoto1 = document.getElementById('fbphoto1');
+//   var fbphoto2 = document.getElementById('fbphoto2');
+//   fbphoto1.style.display="block";
+//   fbphoto2.style.display="block";
+//   document.getElementById("showmorephotosfacebook").innerHTML = "( - ) hide these photos!";
+// }
 
-function toggleImages() {
-  var yes = document.getElementById('fbphoto1');
-  var yes2 = document.getElementById('fbphoto2');
-  if (yes.style.display=="block" && yes2.style.display=="block") {
-    hide();
-  }
-  else {
-    show();
-  }
-}
+// function toggleImages() {
+//   var yes = document.getElementById('fbphoto1');
+//   var yes2 = document.getElementById('fbphoto2');
+//   if (yes.style.display=="block" && yes2.style.display=="block") {
+//     hide();
+//   }
+//   else {
+//     show();
+//   }
+// }
 
-function hidePlaces() {
-  var place = document.getElementById('places');
-  place.style.display="none";
-  document.getElementById("showplaces").innerHTML = "( + ) show me where to go!";
-}
+// function hidePlaces() {
+//   var place = document.getElementById('places');
+//   place.style.display="none";
+//   document.getElementById("showplaces").innerHTML = "( + ) show me where to go!";
+// }
 
-function showPlaces() {
-  var place = document.getElementById('places');
-  place.style.display="block";
-  document.getElementById("showplaces").innerHTML = "( - ) I've seen enough for now!";
-}
+// function showPlaces() {
+//   var place = document.getElementById('places');
+//   place.style.display="block";
+//   document.getElementById("showplaces").innerHTML = "( - ) I've seen enough for now!";
+// }
 
-function togglePlaces() {
-  var okay = document.getElementById('places');
-  if (okay.style.display=="block") {
-    hidePlaces();
-  }
-  else {
-    showPlaces();
-  }
-}
+// function togglePlaces() {
+//   var okay = document.getElementById('places');
+//   if (okay.style.display=="block") {
+//     hidePlaces();
+//   }
+//   else {
+//     showPlaces();
+//   }
+// }
 
-function hideFoods() {
-  var food = document.getElementById('foods');
-  food.style.display="none";
-  document.getElementById("showfoods").innerHTML = "( + ) show me the food!";
-}
+// function hideFoods() {
+//   var food = document.getElementById('foods');
+//   food.style.display="none";
+//   document.getElementById("showfoods").innerHTML = "( + ) show me the food!";
+// }
 
-function showFoods() {
-  var food = document.getElementById('foods');
-  food.style.display="block";
-  document.getElementById("showfoods").innerHTML = "( - ) maybe later...";
-}
+// function showFoods() {
+//   var food = document.getElementById('foods');
+//   food.style.display="block";
+//   document.getElementById("showfoods").innerHTML = "( - ) maybe later...";
+// }
 
-function toggleFoods() {
-  var okay = document.getElementById('places');
-  if (okay.style.display=="block") {
-    hideFoods();
-  }
-  else {
-    showFoods();
-  }
-}
+// function toggleFoods() {
+//   var okay = document.getElementById('places');
+//   if (okay.style.display=="block") {
+//     hideFoods();
+//   }
+//   else {
+//     showFoods();
+//   }
+// }
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
 
-function hideDrop() {
-  var drop = document.getElementById('dropdown-content');
-  drop.style.display="none";
-  document.getElementById("dropbtn").innerHTML = "MORE! &darr;";
-}
+// function hideDrop() {
+//   var drop = document.getElementById('dropdown-content');
+//   drop.style.display="none";
+//   document.getElementById("dropbtn").innerHTML = "MORE! &darr;";
+// }
 
-function showDrop() {
-  var drop = document.getElementById('dropdown-content');
-  drop.style.display="block";
-  document.getElementById("dropbtn").innerHTML = "hello;";
-}
+// function showDrop() {
+//   var drop = document.getElementById('dropdown-content');
+//   drop.style.display="block";
+//   document.getElementById("dropbtn").innerHTML = "hello;";
+// }
 
-function toggleDrop() {
-  var hello = document.getElementById('dropdown-content');
-  if (hello.style.display=="block") {
-    hideDrop();
-  }
-  else {
-    showDrop();
-  }
-}
+// function toggleDrop() {
+//   var hello = document.getElementById('dropdown-content');
+//   if (hello.style.display=="block") {
+//     hideDrop();
+//   }
+//   else {
+//     showDrop();
+//   }
+// }
 
 function hideCaliPlaces() {
   var caliPlace = document.getElementById('caliplaces');
@@ -150,5 +152,36 @@ function toggleCaliFoods() {
   }
   else {
     showCaliFoods();
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function hideDrop() {
+  var drop = document.getElementById('dropdown-content');
+  drop.style.display="none";
+  document.getElementById("dropbtn").innerHTML = "MORE! &darr;";
+}
+
+function showDrop() {
+  var drop = document.getElementById('dropdown-content');
+  drop.style.display="block";
+  document.getElementById("dropbtn").innerHTML = "hello;";
+}
+
+function toggleDrop() {
+  var hello = document.getElementById('dropdown-content');
+  if (hello.style.display=="block") {
+    hideDrop();
+  }
+  else {
+    showDrop();
   }
 }
