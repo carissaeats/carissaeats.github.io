54b5477f80bb419a36cc2626735cada8377cc55c
carissaeats <58920518+carissaeats@users.noreply.github.com>
2020-04-13 13:10:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ window.onload = function(){
   hideFoods();
   hidePlaces();
   hideDrop();
+  hideCaliPlaces();
+  hideCaliFoods();
 }
 
 function hide() {
@@ -104,5 +106,49 @@ function toggleDrop() {
   }
   else {
     showDrop();
+  }
+}
+
+function hideCaliPlaces() {
+  var caliPlace = document.getElementById('caliplaces');
+  caliPlace.style.display="none";
+  document.getElementById("showcaliplaces").innerHTML = "( + ) show me where to go!";
+}
+
+function showCaliPlaces() {
+  var caliPlace = document.getElementById('caliplaces');
+  caliPlace.style.display="block";
+  document.getElementById("showcaliplaces").innerHTML = "( - ) I've seen enough for now!";
+}
+
+function toggleCaliPlaces() {
+  var cali = document.getElementById('caliplaces');
+  if (cali.style.display=="block") {
+    hideCaliPlaces();
+  }
+  else {
+    showCaliPlaces();
+  }
+}
+
+function hideCaliFoods() {
+  var caliFood = document.getElementById('califoods');
+  caliFood.style.display="none";
+  document.getElementById("showcalifoods").innerHTML = "( + ) show me the food!";
+}
+
+function showCaliFoods() {
+  var caliFood = document.getElementById('califoods');
+  caliFood.style.display="block";
+  document.getElementById("showcalifoods").innerHTML = "( - ) maybe later...";
+}
+
+function toggleCaliFoods() {
+  var cali = document.getElementById('califoods');
+  if (cali.style.display=="block") {
+    hideCaliFoods();
+  }
+  else {
+    showCaliFoods();
   }
 }
