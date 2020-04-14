ada02680b358bf2c8858c4b0bd522fe7baa1ec87
carissaeats <58920518+carissaeats@users.noreply.github.com>
2020-04-14 02:17:28 -0400

    Update caliscript.js

--- scriptfiles/caliscript.js
+++ scriptfiles/caliscript.js
@@ -1,7 +1,6 @@
 window.onload = function(){
     hideCaliFoods();
     hideCaliPlaces();  
-//     hideDrop();
 }
 
 function hideCaliPlaces() {
@@ -47,34 +46,3 @@ function toggleCaliFoods() {
         showCaliFoods();
     }
 }
-
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
-
-// function hideDrop() {
-//     var drop = document.getElementById('dropdown-content');
-//     drop.style.display="none";
-//     document.getElementById("dropbtn").innerHTML = "MORE! &darr;";
-// }
-
-// function showDrop() {
-//     var drop = document.getElementById('dropdown-content');
-//     drop.style.display="block";
-//     document.getElementById("dropbtn").innerHTML = "hello";
-// }
-
-// function toggleDrop() {
-//     var hello = document.getElementById('dropdown-content');
-//     if (hello.style.display=="block") {
-//         hideDrop();
-//     }
-//     else {
-//         showDrop();
-//     }
-// }
